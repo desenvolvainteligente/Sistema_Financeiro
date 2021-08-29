f2f37900bd55a6436d72b009c4784f253d298ff4
desenvolvainteligente <desenvolvainteligente@gmail.com>
2021-08-29 10:41:58 -0300

    Criação do layout main, pastas de estilo e script

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- style bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="css/style.css">
+
+        <!-- Script do site -->
+        <script src="js/script.js"></script>
+
+    </head>
+    <body class="antialiased">
+        @yield('content')
+    </body>
+</html>
